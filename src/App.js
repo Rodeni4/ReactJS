a190c52dd0c4404dba788e83963336f992d1c4f4
Sergey <rodenkosv@gmail.com>
2024-04-04 01:04:19 +0700

    Homework 1, adding files and code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import React from 'react';
 import './App.css';
+import Message from './components_hw1/Message';
 
 function App() {
   return (
     <div className="App">
       <h2>Start app</h2>
+      <h3>Homework 1</h3>
+      <Message text='Hello World!' />
     </div>
   );
 }
